f6360651710cc49d2743159aad5877de2a78dff9
Benjamin Höglinger <nefarius@dhmx.at>
2016-11-06 20:21:15 +0100

    Added hidblock driver skeleton

---
+++ hidblock/Driver.h
@@ -0,0 +1,36 @@
+/*++
+
+Module Name:
+
+    driver.h
+
+Abstract:
+
+    This file contains the driver definitions.
+
+Environment:
+
+    User-mode Driver Framework 2
+
+--*/
+
+#define INITGUID
+
+#include <windows.h>
+#include <wdf.h>
+
+#include "device.h"
+#include "queue.h"
+#include "trace.h"
+
+EXTERN_C_START
+
+//
+// WDFDRIVER Events
+//
+
+DRIVER_INITIALIZE DriverEntry;
+EVT_WDF_DRIVER_DEVICE_ADD hidblockEvtDeviceAdd;
+EVT_WDF_OBJECT_CONTEXT_CLEANUP hidblockEvtDriverContextCleanup;
+
+EXTERN_C_END
